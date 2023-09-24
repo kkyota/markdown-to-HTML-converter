a6d5af87f2827638d852ffed39bae38317add7ce
Karasawa Kyota <karakyo1216@gmail.com>
2023-09-24 21:52:08 +0900

    added errorcheck

--- markdown-to-HTML-converter.py
+++ markdown-to-HTML-converter.py
@@ -1,15 +1,24 @@
 import sys
 import markdown
 
-markdown_file = sys.argv[2]
-html_file = sys.argv[3]
+if len(sys.argv) != 4:
+    print("Usage: python3 markdown-to-HTML-converter.py input.md output.html")
+    sys.exit(1)
 
-with open(markdown_file, 'r') as file:
-    markdown_text = file.read()
+markdown_file = sys.argv[1]
+html_file = sys.argv[2]
 
-html_text = markdown.markdown(markdown_text)
+try:
+    with open(markdown_file, 'r') as file:
+        markdown_text = file.read()
 
-with open(html_file, 'w') as file:
-    file.write(html_text)
+    html_text = markdown.markdown(markdown_text)
 
-print("変換が完了しました。")
+    with open(html_file, 'w') as file:
+        file.write(html_text)
+
+    print("変換が完了しました。")
+except FileNotFoundError:
+    print(f"エラー: ファイル '{markdown_file}' が見つかりません。")
+except Exception as e:
+    print(f"エラー: {e}")
